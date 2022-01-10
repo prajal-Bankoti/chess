0a639eba92337e5dc4a60fb773144c03139d1ffa
prjal-Bankoti <prajalbankoti971@gmail.com>
2022-01-10 16:40:45 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from "./logo.svg";
 import "./App.css";
-
 function App() {
+  const data = [];
+  for (let i = 1; i <= 8; i++) {
+    data.push(i);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      {data.map((e) => (
+        <>
+          <div
+            style={{ background: e % 2 === 0 ? "white" : "black" }}
+            className="box-1"
+          ></div>
+          <div
+            style={{ background: e % 2 === 0 ? "black" : "white" }}
+            className="box-1"
+          ></div>
+          <div
+            style={{ background: e % 2 === 0 ? "white" : "black" }}
+            className="box-1"
+          ></div>
+          <div
+            style={{ background: e % 2 === 0 ? "black" : "white" }}
+            className="box-1"
+          ></div>
+          <div
+            style={{ background: e % 2 === 0 ? "white" : "black" }}
+            className="box-1"
+          ></div>
+          <div
+            style={{ background: e % 2 === 0 ? "black" : "white" }}
+            className="box-1"
+          ></div>
+          <div
+            style={{ background: e % 2 === 0 ? "white" : "black" }}
+            className="box-1"
+          ></div>
+          <div
+            style={{ background: e % 2 === 0 ? "black" : "white" }}
+            className="box-1"
+          ></div>
+        </>
+      ))}
     </div>
   );
 }
-
 export default App;
